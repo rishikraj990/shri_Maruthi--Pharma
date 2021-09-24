df00f5bb46f49e4b479be30811e8c3b2a729b673
rishikraj990 <91304008+rishikraj990@users.noreply.github.com>
2021-09-24 09:54:07 +0530

    Add initial version of the website

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector("button").addEventListener("click",function(){
+  alert("i got clicked")
+});
+
+function handleClick(){
+
+}
+
+
+alert("We want to know your location for better experiance");
